2b09f86bcd107d8e22720b37efb0ab177a2ef05d
reconman <reconman@users.noreply.github.com>
2021-02-12 23:01:02 +0100

    Format anime-titles after download

--- update.bat
+++ update.bat
@@ -2,6 +2,7 @@
 
 curl -A Scudlee/anime-lists -o animetitles.xml --compressed http://anidb.net/api/anime-titles.xml.gz
 
+xmllint --format --output animetitles.xml animetitles.xml
 xsltproc -o animetitles.xml transforms/sort-animetitles.xsl animetitles.xml
 xsltproc -o anime-list-master.xml transforms/update-anime-list-master.xsl animetitles.xml
 call generate-lists.bat
